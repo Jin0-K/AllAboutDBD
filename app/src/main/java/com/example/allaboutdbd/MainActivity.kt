4c7b39ce935a26bffd7d569f530ea9e4e65d97a4
Arang King <jykim0827@gmail.com>
2022-08-31 15:45:19 +0900

    Created MainMenuActivity
    MainActivity shows when the app launches, then to MainMenuActivity automatically

--- app/src/main/java/com/example/allaboutdbd/MainActivity.kt
+++ app/src/main/java/com/example/allaboutdbd/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.allaboutdbd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        startMainMenu()
+    }
+
+    fun startMainMenu() {
+        if (!isDestroyed) {
+            val intent = Intent(this@MainActivity, MainMenuActivity::class.java)
+            //the task to do after timer ends
+            val timertask = timerTask {
+                if (!isDestroyed) {
+                    startActivity(intent)
+                    finish()
+                }
+            }
+
+            //Initialize timer
+            val timer = Timer()
+            timer.schedule(timertask, 2000)
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/allaboutdbd/MainMenuActivity.kt
@@ -0,0 +1,14 @@
+package com.example.allaboutdbd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+
+    }
+
+
+}
